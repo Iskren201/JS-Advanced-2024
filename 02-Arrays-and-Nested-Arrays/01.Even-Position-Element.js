4afc7c6ed7b0f959e2347f9b6f03a0d6ec1dce1a
Iskren Iliev <iskren201@gmail.com>
2024-01-19 12:55:58 +0200

    commit

--- 02-Arrays-and-Nested-Arrays/01.Even-Position-Element.js
+++ 02-Arrays-and-Nested-Arrays/01.Even-Position-Element.js
@@ -1,1 +1,7 @@
-function slove(input) {}
+function slove(input) {
+  for (var i = 0; i < input.length; i += 2) {
+    console.log(result[i]);
+  }
+}
+
+slove(["20", "30", "40", "50", "60"]);
---
+++ 02-Arrays-and-Nested-Arrays/02.Last-K-Numbers-Sequence.js
@@ -0,0 +1,18 @@
+function slove(n, k) {
+  let sequence = [1];
+
+  for (let i = 1; i < n; i++) {
+    let sum = 0;
+    let startIndex = Math.max(0, i - k);
+
+    for (let j = startIndex; j < i; j++) {
+      sum += sequence[j];
+    }
+
+    sequence.push(sum);
+  }
+
+  return sequence;
+}
+
+console.log(slove(6, 3));
