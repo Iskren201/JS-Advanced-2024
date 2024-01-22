2aeff785e9ea6194700682ee760acf5802f4beca
Iskren201 <iskren201@gmail.com>
2024-01-22 22:58:22 +0200

    commit

--- DOM-Introduction/02.Collect List Items/CollectListItems.js
+++ DOM-Introduction/02.Collect List Items/CollectListItems.js
@@ -1,3 +1,12 @@
 function extractText() {
-    // TODO
+  const list = document.getElementById("items");
+  let reslut = [];
+
+  const items = Array.from(list.children);
+  for (let item of items) {
+    reslut.push(item.textContent);
+  }
+
+  const textArea = document.getElementById("result");
+  textArea.value = reslut.join("\n");
 }
--- DOM-Introduction/03.Sum Numbers/SumNumbers.js
+++ DOM-Introduction/03.Sum Numbers/SumNumbers.js
@@ -1,3 +1,9 @@
 function calc() {
-    // TODO: sum = num1 + num2
+  // TODO: sum = num1 + num2
+
+  let FirstNumber = document.getElementById("num1").value;
+  let SecundNumber = document.getElementById("num2").value;
+
+  const sum = Number(FirstNumber) + Number(SecundNumber);
+  document.getElementById("sum").value = sum;
 }
