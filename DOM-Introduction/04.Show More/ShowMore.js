eb8cc450f06921004a68b8ed282d22f5d99f53df
Iskren201 <iskren201@gmail.com>
2024-01-22 23:02:03 +0200

    Update ShowMore.js

--- DOM-Introduction/04.Show More/ShowMore.js
+++ DOM-Introduction/04.Show More/ShowMore.js
@@ -1,3 +1,18 @@
 function showText() {
-    // TODO
+  // Get the text element by its ID
+  var textElement = document.getElementById("text");
+
+  // Toggle the display style between 'none' and 'inline' (or any other desired style)
+  if (
+    textElement.style.display === "none" ||
+    textElement.style.display === ""
+  ) {
+    textElement.style.display = "inline";
+  } else {
+    textElement.style.display = "none";
+  }
+
+  // Hide the "Read more" link after clicking
+  var moreLink = document.getElementById("more");
+  moreLink.style.display = "none";
 }
