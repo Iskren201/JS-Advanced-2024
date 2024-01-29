cd9a02971cfb3ffed232b9d929347378b8abfe5e
Iskren201 <iskren201@gmail.com>
2024-01-29 10:36:02 +0200

    Update app.js

--- 05-DOM-Events-Lab/05. Highlight-Active/app.js
+++ 05-DOM-Events-Lab/05. Highlight-Active/app.js
@@ -1,3 +1,24 @@
 function focused() {
-    console.log('TODO:...');
+  const divsReff = document.querySelectorAll("div div");
+  const gradient = document.querySelectorAll('div div [type="text"]');
+
+  let gradientArr = Array.from(gradient);
+  for (const input of gradientArr) {
+    input.addEventListener("click", highlightActiveDiv);
+    input.addEventListener("blur", removeFocus);
+  }
+  function highlightActiveDiv(event) {
+    const focusedInput = event.parentElement;
+
+    const parentDiv = focusedInput.closest("div");
+    if (parentDiv) {
+      parentDiv.classList.add("focused");
+    }
+  }
+  function removeFocus(event) {
+    let arrayDivs = Array.from(divsReff);
+    for (let div of arrayDivs) {
+      div.classList.remove("focused");
+    }
+  }
 }
